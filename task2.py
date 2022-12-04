ff5675d8505f7e2e207e579329c1ed1984799fed
Ruslan Sakaev <hardist@mail.ru>
2022-12-04 15:21:49 +0300

    Добавл решение первой задачи

--- task2.py
+++ task2.py
@@ -3,3 +3,13 @@
 # *Пример:*
 # - [2, 3, 4, 5, 6] =>[12,15,16]      ([2*6, 3*5, 4*4]);
 # - [2, 3, 5, 6] => [12,15]   ( [2*6, 3*5]) 
+
+def pair_mult(nums: list[int]) -> list:
+    pairs = []
+    iterations = int(round((len(nums)+1)/2))
+    print(iterations)
+    for i in range(iterations):
+        pairs.append(nums[i]*nums[-1-i])
+    return pairs
+
+print(pair_mult([2, 3, 5, 6]))
